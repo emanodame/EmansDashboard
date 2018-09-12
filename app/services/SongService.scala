a6ba8d62e5cb9fbd850dc26b6e93efcdeb55e6bc
emanodame <emanodame1@gmail.com>
2018-09-13 00:36:31 +0100

    improved security by extracting out keys

--- app/services/SongService.scala
+++ app/services/SongService.scala
@@ -2,14 +2,14 @@ package services
 
 import domain.Song
 import javax.inject.Inject
+import play.api.Configuration
 import play.api.cache.SyncCacheApi
 import util.CustomIO
 import play.api.libs.json.{Json, _}
 import response.SongResponse
 
-final class SongService @Inject()(cache: SyncCacheApi) {
-  private val apiKey = "V4BQYEA_PgtWJW3SV7_G9qiV2yOA4WbwKGBVoPl5ROCL4SFWFvGzYQccLtpU6Hro"
-  private val apiSearchLink = "https://api.genius.com/search?q="
+final class SongService @Inject()(cache: SyncCacheApi, config: Configuration) {
+  private val apiKey = config.get[String]("songService.apiKey")
 
   def getSongInfo(songName: String): SongResponse = {
     cache.get[Song](songName) match {
@@ -49,14 +49,14 @@ final class SongService @Inject()(cache: SyncCacheApi) {
 
   private def generateSearchLink(songName: String): String = {
     val formattedSongName = songName.replace(" ", "%20")
-    apiSearchLink.concat(formattedSongName)
+    "https://api.genius.com/search?q=".concat(formattedSongName)
   }
 
   private def retrieveSong(songId: JsValue): Either[String, JsValue] = {
     val searchLink = "https://api.genius.com/songs/" + songId
 
-    val geniusStringResponse = CustomIO.getHtmlFromWebsiteViaHttp(searchLink, apiKey)
-    geniusStringResponse.attempt
+    val geniusIoMonad = CustomIO.getHtmlFromWebsiteViaHttp(searchLink, apiKey)
+    geniusIoMonad.attempt
       .unsafeRunSync()
       .fold(_ => Left("Failure; Check network connectivity"),
         retrievedJson => Right((Json.parse(retrievedJson) \ "response" \ "song").get))
